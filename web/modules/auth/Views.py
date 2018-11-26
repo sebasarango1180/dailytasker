96831d22feb165441e3e786dce3fbe01219c2c53
sebasarango1180 <sebasarango1180@gmail.com>
2018-11-26 05:46:58 -0500

    Created registration endpoint

--- web/modules/auth/Views.py
+++ web/modules/auth/Views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from http import HTTPStatus
 import mongoengine
-import logging
+import logging, datetime
 
 from web import config
 from web.modules.auth.Models import User
@@ -26,7 +26,25 @@ def login():
             if not result_user['confirmed']:
                 return jsonify({'login': False, 'msg': 'User not confirmed'})
 
-            return jsonify({'login': True, 'token': encoded_jwt(result_user), 'uid': str(result_user['_id']),
-                            'email': result_user['email']})
+            return jsonify({'login': True, 'token': encoded_jwt(result_user), 'user_id': str(result_user['_id']),
+                            'name': result_user['name']})
 
     return jsonify({'login': False, 'msg': 'wrong username or password'}), HTTPStatus.BAD_REQUEST
+
+
+@authBlueprint.route('/register', methods=['POST'])
+def register():
+    data = request.get_json() if request.get_json() else dict()
+
+    if not data['email'] or data['name']:
+        return jsonify({'error': 'Missing required fields'}), HTTPStatus.BAD_REQUEST
+
+    data['registration_date'] = datetime.datetime.utcnow()
+
+    try:
+        new_user = User(**data).save()
+
+        return jsonify({'created': True, 'token': encoded_jwt(new_user), 'name': data['name']}), HTTPStatus.CREATED
+
+    except Exception as e:
+        return jsonify({'created': False, 'msg': str(e)}), HTTPStatus.BAD_REQUEST
