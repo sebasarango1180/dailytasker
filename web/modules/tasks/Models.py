cdddc45b8567c7f1ca00fd700b253f63c92cb0f4
sebasarango1180 <sebasarango1180@gmail.com>
2018-11-28 06:34:02 -0500

    Refactor for Task model

--- web/modules/tasks/Models.py
+++ web/modules/tasks/Models.py
@@ -1,11 +1,23 @@
-import mongoengine as me
+import os
 from datetime import datetime
+from uuid import uuid4
 
+import mongoengine as me
+from bson import ObjectId
 
+STATUS_OPTIONS = {
+    'TODO': 'To Do',
+    'IN_PROGRESS': 'In Progress',
+    'DONE': 'Done'
+}
 class Task(me.Document):
-    user_id = me.ObjectIdField(required=True)
     description = me.StringField(required=True)
-    status = me.StringField(required=True)
+    status = me.StringField(required=True, default=STATUS_OPTIONS['TODO'])
     creation_date = me.DateTimeField(required=True, default=datetime.utcnow())
     completion_date = me.DateTimeField(required=False, default=None)
     due_date = me.DateTimeField(required=False, default=None)
+
+    meta = {
+        'strict': False,
+        'collection': 'tasks'
+    }
